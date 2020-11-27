0b5ace09256e1d99bf19093b1fbd090a925fe8e9
augustolachini <augustolachini@gmail.com>
2020-11-27 14:20:04 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,jsonify,request
+from joblib import load
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'query' not in request.args:
+        return jsonify({
+            'prediction':None,
+            'message':'sendme a txt'
+        })
+    query = request.args.get('query')
+    model = load('model.joblib')
+    labels = ['carros' 'economia' 'educacao' 'esporte' 'musica' 'politica']
+    
+    predict = model.predict([query])
+    prediction = labels[predict[0]]
+    
+    return jsonify({'prediction': prediction})
